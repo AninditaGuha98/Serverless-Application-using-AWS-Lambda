be436d86c7cd88b9ed4beecbe613ac6ffb8ea851
Anindita Guha <an711755@dal.ca>
2020-06-24 13:44:51 -0300

    lambda function 1 completed and tested

--- extractFeatures.py
+++ extractFeatures.py
@@ -1,27 +1,30 @@
-author = "Anindita"
-
+import json
 import nltk
-from nltk.tokenize import word_tokenize
-from nltk.tag import pos_tag
+import boto3
+
+nltk.data.path.append('/opt/nltk_data')
 
 
+def lambda_handler(event, context):
+    if event:
+        s3 = boto3.client('s3')
+        file_object = event["Records"][0]
+        file = str(file_object['s3']['object']['key'])
+        data = s3.get_object(Bucket='sampledatab00845637', Key=file)
+        contents = data['Body'].read().decode('utf-8')
 
-def extractFeatures(file):
-    entity={}
-    word = nltk.word_tokenize(file)
-    word = nltk.pos_tag(word)
-    for item in word:
-        if item[1]=='NNP':
-            if item[0] in entity:
-                entity[item[0]]+=1
-            else:
-                entity[item[0]]=1
-    print(entity)
+        entity = {}
+        word = nltk.word_tokenize(contents)
+        word = nltk.pos_tag(word)
+        for item in word:
+            if item[1] == 'NNP':
+                if item[0] in entity:
+                    entity[item[0]] += 1
+                else:
+                    entity[item[0]] = 1
+        print(entity)
 
-
-
-
-with open("001.txt") as file_read:
-    contents = file_read.read()
-    extractFeatures(contents)
-    input()
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
--- uploadFiletoS3.py
+++ uploadFiletoS3.py
@@ -10,6 +10,8 @@ s3 = boto3.client('s3')
 counter= 1
 for file in glob.glob('tech/*.txt'):
     s3.upload_file(file, "sampledatab00845637", str(counter)+'.txt')
+    print("upload successful", counter)
     counter=counter+1
     time.sleep(0.1)
     input()
+
