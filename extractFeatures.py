ad75757177f197d98cf3b6e94eab09852dc3cbfd
Anindita Guha <an711755@dal.ca>
2020-06-24 14:32:48 -0300

    1st Lambda function refactored

--- extractFeatures.py
+++ extractFeatures.py
@@ -1,8 +1,38 @@
+author = "Anindita"
+
 import json
 import nltk
 import boto3
 
 nltk.data.path.append('/opt/nltk_data')
+
+
+def namedEntity(contents, file):
+    entity = {}
+    s3File = {}
+    word = nltk.word_tokenize(contents)
+    word = nltk.pos_tag(word)
+    for item in word:
+        if item[1] == 'NNP':
+            if item[0] in entity:
+                entity[item[0]] += 1
+            else:
+                entity[item[0]] = 1
+    print(entity)
+
+    names = file.split('.')
+    key = names[0] + 'ne'
+    filename = names[0] + 'ne.' + names[1]
+    s3File[key] = entity
+    uploadJSONtoS3(s3File, filename)
+
+
+def uploadJSONtoS3(s3File, filename):
+    s3 = boto3.resource('s3')
+    s3object = s3.Object('tagsb00845637', filename)
+    s3object.put(
+        Body=(bytes(json.dumps(s3File).encode('UTF-8')))
+    )
 
 
 def lambda_handler(event, context):
@@ -12,19 +42,11 @@ def lambda_handler(event, context):
         file = str(file_object['s3']['object']['key'])
         data = s3.get_object(Bucket='sampledatab00845637', Key=file)
         contents = data['Body'].read().decode('utf-8')
-
-        entity = {}
-        word = nltk.word_tokenize(contents)
-        word = nltk.pos_tag(word)
-        for item in word:
-            if item[1] == 'NNP':
-                if item[0] in entity:
-                    entity[item[0]] += 1
-                else:
-                    entity[item[0]] = 1
-        print(entity)
-
+        namedEntity(contents, file)
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
     }
+
+
+
